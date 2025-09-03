321e8a99b881f9951cc3dde3399036e47beb0eaf
Your Name <Nada Elbadry nadaelbadry1012@gmail.com>
2025-09-03 23:03:11 +0300

    Add project files.

---
+++ Demo_Adv_04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo_Adv_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
